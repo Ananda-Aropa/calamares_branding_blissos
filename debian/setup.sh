a3e27a95db8627d1f5ffde6e1e85f4746d9408af
Shadichy <shadichy@blisslabs.org>
2025-07-12 15:25:08 +0700

    ci: setup: Generalize package name
    Signed-off-by: Shadichy <shadichy@blisslabs.org>

--- debian/setup.sh
+++ debian/setup.sh
@@ -10,13 +10,15 @@ MAINTAINER=$(git log -1 --pretty=format:'%an <%ae>')
 VERSION=$(cat ../VERSION | cut -d'v' -f2)
 REVISION=${REVISION:-0}
 
+PACKAGE_NAME=$(grep 'Source:' debian/control | cut -d' ' -f2)
+
 # Gen changelog (from latest commit)
 MSG=$(git log -1 --pretty=format:'%s')
 DATE=$(git log -1 --pretty=format:'%ad' --date=format:'%a, %d %b %Y %H:%M:%S %z')
 
 # Generate changelog
 cat <<EOF >changelog
-calamares-settings-blissos ($VERSION-$REVISION) $DISTRO; urgency=medium
+$PACKAGE_NAME ($VERSION-$REVISION) $DISTRO; urgency=medium
 
 $(echo -e "$MSG" | sed -r 's/^/  * /g')
 
